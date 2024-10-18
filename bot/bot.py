814fcb8d8d2db48e6890a309fcd982d8665130db
matkat1 <72603220+matkat1@users.noreply.github.com>
2024-10-18 19:16:31 +0200

    build slots system added
    bot will now recognize a few spots appropriate for 3x3 buildings with add-ons within the main base and place its buildings there

--- bot/bot.py
+++ bot/bot.py
@@ -9,31 +9,25 @@ from sc2.position import Point2
 from sc2.player import Bot, Computer
 
 
+
 class CompetitiveBot(BotAI):
     NAME: str = "MacroPolo"
-    """This bot's name"""
-
     RACE: Race = Race.Terran
-    """This bot's Starcraft 2 race.
-    Options are:
-        Race.Terran
-        Race.Zerg
-        Race.Protoss
-        Race.Random
-    """
+
+    building_slots: []
 
     async def on_start(self):
-        """
-        This code runs once at the start of the game
-        Do things here before the game starts
-        """
+        
+        base = self.townhalls.ready.random
+        main_position = base.position.towards(self.enemy_start_locations[0], 15)
+        main_ramp = await self.find_main_ramp(main_position)
+        self.building_slots = await self.find_building_slots(main_ramp)
         print("Game started")
+        
+
 
     async def on_step(self, iteration: int):
-        """
-        This code runs continually throughout the game
-        Populate this function with whatever your bot should do!
-        """
+        
         await self.distribute_workers()
         await self.build_workers()
         await self.build_supply()
@@ -42,6 +36,112 @@ class CompetitiveBot(BotAI):
         await self.build_marines()
         await self.attack()
         pass
+    
+    async def find_building_slots(self, main_ramp):
+        base = self.townhalls.ready.random
+        main_position = base.position.towards(self.enemy_start_locations[0], 15)
+        start = []
+        start.append(base.position[0])
+        start.append(base.position[1])
+        xd=main_ramp.barracks_correct_placement
+        tmp2=[2,2]
+        tmp2[0]=xd[0]
+        tmp2[1]=xd[1]
+        slots=[main_ramp.barracks_correct_placement]
+        add=1
+        point = Point2((tmp2[0], (tmp2[1] + (add * 3)) ))
+        while(await self.check_build_spot(point)):
+             slots.append(Point2((tmp2[0],(tmp2[1]+(add*3)))))
+             add=add+1
+             point = Point2((tmp2[0], (tmp2[1] + (add * 3)) ))
+        add=1
+        point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+        while(await self.check_build_spot(point)):
+             slots.append(Point2((tmp2[0],(tmp2[1]-(add*3)))))
+             add=add+1
+             point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+        add=0
+        tmp2[0]-=7
+        point = Point2((tmp2[0], (tmp2[1] + (add * 3)) ))
+        if(start[1]>main_position[1]):
+          for i in range(4):
+              if (await self.check_build_spot(point)):
+                 slots.append(Point2((tmp2[0],(tmp2[1]+(add*3)))))
+              add=add+1
+              point = Point2((tmp2[0], (tmp2[1] + (add * 3)) ))
+        add=0
+        point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+        if(start[1]<main_position[1]):
+          for i in range(4):
+               if (await self.check_build_spot(point)):
+                 slots.append(Point2((tmp2[0],(tmp2[1]-(add*3)))))
+               add=add+1
+               point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+        add=0
+        tmp2[0]+=14
+        if(start[1]>main_position[1]):
+          for i in range(4):
+               if (await self.check_build_spot(point)):
+                 slots.append(Point2((tmp2[0],(tmp2[1]+(add*3)))))
+               add=add+1
+               point = Point2((tmp2[0], (tmp2[1] + (add * 3)) ))
+        add=0
+        if(start[1]<main_position[1]):
+          point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+          for i in range(4):
+               if (await self.check_build_spot(point)):
+                 slots.append(Point2((tmp2[0],(tmp2[1]-(add*3)))))
+               add=add+1
+               point = Point2((tmp2[0], (tmp2[1] - (add * 3)) ))
+        return slots    
+
+    async def draw_main_asci(self):
+     
+        x1=int(self.start_location[0])
+        y1=int(self.start_location[1])
+        base=self.start_location
+        pos = base.position.towards(self.enemy_start_locations[0], 15)
+        pos=(int(pos[0]),int(pos[1]))
+        x2=int(pos[0])
+        y2=int(pos[1])
+        for x in range (60):
+            for y in range (60):
+                 if (x==30 or y==30):
+                      print("?", end="")
+                 elif (x1+30-x==x2 or y1+30-y==y2):
+                      print("!", end="")
+                 elif self.game_info.placement_grid.is_set((x1+30-x,y1+30-y)):
+                      print("#", end="")
+                 else:
+                      print(" ", end="")
+            print("")
+
+    async def find_main_ramp(self, main):
+        ramps=self.game_info._find_ramps_and_vision_blockers()
+        ramps=ramps[0]
+        min=99999
+        main_ramp=ramps[0]
+        for ramp in ramps:
+             dist=main.distance_to(ramp.top_center)
+             if (dist<min):
+                  min = dist
+                  main_ramp = ramp
+        return main_ramp
+         
+    async def check_build_spot(self, pos: Point2):
+         grid=self.game_info.placement_grid
+         x1=pos[0]
+         y1=pos[1]
+         check=True
+         for x in range (7):
+              for y in range(3):
+                   if(not grid.is_set((int(x1-3+x), int(y1-1+y)))):
+                        check=False
+                        break
+         if (check):
+              return True
+         else: 
+              return False
 
     async def build_workers(self):
             bases=self.townhalls.ready
@@ -52,20 +152,28 @@ class CompetitiveBot(BotAI):
                     base.train(UnitTypeId.SCV)
 
     async def build_supply(self):
-         base = self.townhalls.ready.random
-         pos = base.position.towards(self.enemy_start_locations[0], 10)
+         base=self.start_location
+         pos = base.position.towards(self.enemy_start_locations[0], -15)
          if (self.supply_left < 3 
              and self.already_pending(UnitTypeId.SUPPLYDEPOT)==0
              and self.can_afford(UnitTypeId.SUPPLYDEPOT) ):
               await self.build(UnitTypeId.SUPPLYDEPOT, near=pos)
     
     async def build_rax(self):
-         if (self.structures(UnitTypeId.SUPPLYDEPOT).ready
-             and self.can_afford(UnitTypeId.BARRACKS)
-             and len(self.structures(UnitTypeId.BARRACKS))<3):
-              base = self.townhalls.ready.random
-              pos = base.position.towards(self.enemy_start_locations[0], 20)
-              await self.build(UnitTypeId.BARRACKS, near=pos)
+        if (self.structures(UnitTypeId.SUPPLYDEPOT).ready
+            and self.can_afford(UnitTypeId.BARRACKS)
+            and len(self.structures(UnitTypeId.BARRACKS))<24):
+                for i in range(len(self.building_slots)):
+                    pos=self.building_slots[i]
+                    worker = self.select_build_worker(pos)
+                    if worker is None:
+                         pass
+                    if (await self.can_place_single(UnitTypeId.BARRACKS, pos)):
+                        worker.build(UnitTypeId.BARRACKS, pos)
+                        worker.stop(queue=True)
+                        pass
+                    else:
+                         continue
 
     async def build_gas(self):
          if (self.structures(UnitTypeId.BARRACKS)
@@ -82,10 +190,9 @@ class CompetitiveBot(BotAI):
                              worker.build(UnitTypeId.REFINERY, geyser)
                              worker.stop(queue=True)
               
-    
     async def build_marines(self):
-            raxes=self.structures(UnitTypeId.BARRACKS)
-            for rax in raxes:
+            slots=self.structures(UnitTypeId.BARRACKS)
+            for rax in slots:
                 if (self.can_afford(UnitTypeId.MARINE) 
                     and rax.is_idle):
                     rax.train(UnitTypeId.MARINE)
@@ -94,15 +201,10 @@ class CompetitiveBot(BotAI):
          armycount=self.units(UnitTypeId.MARINE).amount
          marines = self.units(UnitTypeId.MARINE).ready.idle
          for marine in marines:
-              if armycount>20:
+              if armycount>100:
                    marine.attack(self.enemy_start_locations[0])
               
-
     async def on_end(self, result: Result):
-        """
-        This code runs once at the end of the game
-        Do things here after the game ends
-        """
         print("Game ended.")
 
     
